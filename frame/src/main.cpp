331bfbf6b6386d62e5f0837f26abc9c79203d771
imkcy9 <imkcy9@icloud.com>
2017-08-25 10:54:10 +0800

    add main.cpp

---
+++ frame/src/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: chengyeke
+ *
+ * Created on 2017年8月25日, 上午10:53
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
